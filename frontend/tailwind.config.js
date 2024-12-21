a38c2128cc3b1f80f321712d6d6cafca5b9e9bde
nishu28pra <155014211+nishu28pra@users.noreply.github.com>
2024-12-21 12:40:44 +0530

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,20 +1,4 @@
 /** @type {import('tailwindcss').Config} */
-// export default {
-//   content: [
-//     "./index.html",
-//     "./src/**/*.{js,ts,jsx,tsx}",
-//   ],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [
-//     require('daisyui'),
-//   ],
-// };
-
-/** @type {import('tailwindcss').Config} */
-import daisyui from 'daisyui';
-
 export default {
   content: [
     "./index.html",
@@ -27,3 +11,4 @@ export default {
     daisyui,
   ],
 };
+
